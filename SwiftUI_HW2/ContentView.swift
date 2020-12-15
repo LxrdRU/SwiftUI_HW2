90b3eeee0982ff62836db204c346cfe04997631c
Andrei MARKOV <a981203@mail.ru>
2020-12-15 11:48:37 +0300

    Initial Commit

---
+++ SwiftUI_HW2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_HW2
+//
+//  Created by Andrey Markov on 2020-12-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_HW2/SwiftUI_HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HW2App.swift
+//  SwiftUI_HW2
+//
+//  Created by Andrey Markov on 2020-12-15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
